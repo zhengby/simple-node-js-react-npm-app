839b065d4884e5f29680a3ee17bf52cdd8c322ba
biillyzheng <zhengbeiyun@gmail.com>
2018-07-04 04:47:30 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 class App extends Component {
@@ -8,7 +8,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-          <p>hello webhook 1.2.0</p>
+          <p>hello webhook 1.2.1</p>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
